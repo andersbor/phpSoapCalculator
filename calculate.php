7cda9d36fcf5f93a21bef157711b732d4d746bef
andersbor <anbo@easj.dk>
2017-11-09 20:12:05 +0100

    using Azure service

--- calculate.php
+++ calculate.php
@@ -3,11 +3,12 @@
 $a = $_POST['a'];
 $b = $_POST['b'];
 
-$client = new SoapClient("http://localhost:29418/Service1.svc?wsdl");
+$wsdl = "http://anbo-soapprovidercalculator.azurewebsites.net/Service1.svc?wsdl";
+$client = new SoapClient($wsdl);
 $parametersToSoap = array('b' => $b, 'a' => $a);
 $resultWrapped = $client->add($parametersToSoap);
 
-var_dump($resultWrapped);
+print_r($resultWrapped);
 echo "<br />";
 $result = $resultWrapped->AddResult;
 echo $a . "+" . $b . "=" . $result;
